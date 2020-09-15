d595927af0697ca9a2ec54b8bdf7d92b3a571475
lain-Dev <olivier@lebon.dev>
2020-09-15 15:33:48 +0400

    TD shifumi

---
+++ TD - Activité/06 - Shifumi/script/script.js
@@ -0,0 +1,11 @@
+
+function quandMaPageEstCharge() {
+    let nameEnter = window.prompt('Entrer votre nom');
+    console.log(nameEnter);
+    document.getElementById("nomJoueur").innerText = nameEnter;
+
+    const pierre = document.getElementById("img-pierre") = document.getElementById("img-gamer");
+    const feuille = document.getElementById("img-feuille") = document.getElementById("img-gamer");
+    const ciseaux = document.getElementById("img-ciseaux") = document.getElementById("img-gamer");
+    
+};
