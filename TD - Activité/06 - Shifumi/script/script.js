69299db84ea4f19c59872747e92cdfbf1e58db1c
lain-Dev <olivier@lebon.dev>
2020-09-15 21:56:02 +0400

    update shifumi

--- TD - Activité/06 - Shifumi/script/script.js
+++ TD - Activité/06 - Shifumi/script/script.js
@@ -1,11 +1,19 @@
 
 function quandMaPageEstCharge() {
+
+    //Recharger la page
+    function reload(){
+	location.reload();
+    };
+
+    //choix nom
     let nameEnter = window.prompt('Entrer votre nom');
     console.log(nameEnter);
     document.getElementById("nomJoueur").innerText = nameEnter;
 
-    const pierre = document.getElementById("img-pierre") = document.getElementById("img-gamer");
-    const feuille = document.getElementById("img-feuille") = document.getElementById("img-gamer");
-    const ciseaux = document.getElementById("img-ciseaux") = document.getElementById("img-gamer");
+    //choix pierre, feuille, ciseaux
+    const pierre = document.getElementById('img-gamer').src='image/pierre.png';
+    const feuille = document.getElementById('img-gamer').src='image/feuille.png';
+    const ciseaux = document.getElementById('img-gamer').src='image/ciseaux.png';
     
-};
+}
