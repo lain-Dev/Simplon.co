493b61b6a602e3efd85b2ca58f173700859df038
lain-Dev <olivier@lebon.dev>
2020-09-23 15:36:32 +0400

    update nom dossier
    bug quand live server avec vscode

---
+++ TD - Activité/07-Tableau Objet  & Constructeur  Objet/exercice/exercice.js
@@ -0,0 +1,25 @@
+let comptes  = [
+    {
+        nom : "compte courant",
+        solde : 600
+    },
+    {
+        nom : "livret A",
+        solde : 500
+    },
+    {
+        nom : "PEL",
+        solde : 4000
+    },
+    {
+        nom : "LDD",
+        solde : 2500
+    }
+];
+
+//Exercice 01: écrire Algo afficher plus gros montant parmis les comptes dont le résultat attendu 4000 !
+
+
+//Exercice 2: créer une autre personne comptes suivant: Compte courant: 800 et livret A: 1200. stocker dans varialble "comptes2"
+
+
