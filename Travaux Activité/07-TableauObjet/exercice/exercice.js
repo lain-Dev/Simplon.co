6323805df6f33026f8c3654138dfc4eb99e1f789
lain-Dev <olivier@lebon.dev>
2020-09-23 21:21:27 +0400

    update

--- Travaux Activité/07-TableauObjet/exercice/exercice.js
+++ Travaux Activité/07-TableauObjet/exercice/exercice.js
@@ -18,7 +18,7 @@ let comptes  = [
 ];
 
 //Exercice 01: écrire Algo afficher plus gros montant parmis les comptes dont le résultat attendu 4000 !
-
+console.log(comptes[2].solde)
 
 //Exercice 2: créer une autre personne comptes suivant: Compte courant: 800 et livret A: 1200. stocker dans varialble "comptes2"
 
