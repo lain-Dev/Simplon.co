8b75f3b0275f3cb338817430e50d514ba633488f
lain-Dev <olivier@lebon.dev>
2020-10-05 15:27:53 +0400

    update todolist

--- Travaux Activité/11-todo List/scrpit.js
+++ Travaux Activité/11-todo List/scrpit.js
@@ -45,3 +45,10 @@ function newElement() {
 //     `);
 // })
 
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
