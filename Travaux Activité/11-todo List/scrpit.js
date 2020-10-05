3a5d632a6d05339461dacf2dd493e6285d949657
lain-Dev <olivier@lebon.dev>
2020-10-05 12:46:30 +0400

    function additem

--- Travaux Activité/11-todo List/scrpit.js
+++ Travaux Activité/11-todo List/scrpit.js
@@ -1,15 +1,47 @@
-function laDate() {
-    var maintenant=new Date();
-    var jour=maintenant.getDate();
-    var mois=maintenant.getMonth()+1;
-    var an=maintenant.getFullYear();
-    console.log(jour,"/",mois,"/",an); 
-    //document.getElementById("dateDuJour").innerText = maintenant;
+
+//insérer la date
+
+function setDate() {
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+document.getElementById('myDate').value = today;
+} 
+
+//reset page
+function reset() {
+    location.reload();
 }
-laDate();    
 
 
+//insérer item dans la liste
 
+function newElement() {
+    console.log(document.getElementById("myUl"));
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    li.innerHTML = inputValue;
 
+    if (inputValue === '') {
+      alert("entrer une tâche !");
+    } else {
+      document.getElementById("myUl").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+    console.log(document.getElementById("myUl"));
+    
+}    
 
-                    
+// $('#formulaire').on('click', 'button', function () {
+//     $('#myUl').append(`
+//         <li>tesssssst</li>
+//     `);
+// })
+
