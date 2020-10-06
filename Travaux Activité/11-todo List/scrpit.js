a59e346c894b714af3394fef2c6d01db489a695f
lain-Dev <olivier@lebon.dev>
2020-10-06 14:08:11 +0400

    todolist fonctionnel manue la pagination

--- Travaux Activité/11-todo List/scrpit.js
+++ Travaux Activité/11-todo List/scrpit.js
@@ -20,34 +20,66 @@ function reset() {
     location.reload();
 }
 
+//funtion supprimer tache
+function removeItem() {
+  var liste=document.getElementById("myUl").getElementsByTagName("li");
+ 
+for(li in liste){
+	liste[li].onclick=function(){
+    this.parentNode.removeChild(this)
+  }
+  }
+}
 
 //insérer item dans la liste
 
 function newElement() {
-    
+    //crée une balise et donne la classe étoile
     var iconStar = document.createElement('i');
     iconStar.classList ='fas fa-star';
 
+    //récupère la valeur de la section et rajoute la classe en plus pour attribuer une couleur a étoile
+    var selectColor = document.getElementById("star").value;
+    iconStar.classList.toggle(selectColor);
+
+    //crée un btn del et attribu click avec la variable
+    var delBtn = document.createElement('button');
+    var p = document.createElement('p');
+    delBtn.classList = "btn btn-danger btn-sm";
+    delBtn.innerText = 'del';
+    delBtn.addEventListener('click', removeItem);
+    
+    //crée une li et rajoute la valeur input tache a faire
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    li.innerHTML = inputValue;
+    p.innerHTML = inputValue;//si je mets li avant le texte se place en premier avant étoile
 
+    //vérifie qu'une valeur est bien entré pour pas ajouter une tache vide
     if (inputValue === '') {
       alert("entrer une tâche !");
     } else {
+      li.appendChild(iconStar);
+      li.appendChild(p);
+      li.appendChild(delBtn); 
       document.getElementById("myUl").appendChild(li);
     }
+      
+    
+      //version jQuery: exemple créer ma ligne avec code html
+      // $("#myUl").append(`
+      // <li>
+      //     <i class="fas fa-star text-info" id="star"></i>
+      //     <p class="text-info">${inputValue}</p>
+      //     <button class="btn btn-primary btn-sm">del</button>
+      // </li>
+      // `);
+    
 
-    li.appendChild(iconStar);
-    document.getElementById("myInput").value = "";
-        
+   
+
+    
 }    
 
-// $('#formulaire').on('click', 'button', function () {
-//     $('#myUl').append(`
-//         <li>tesssssst</li>
-//     `);
-// })
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
