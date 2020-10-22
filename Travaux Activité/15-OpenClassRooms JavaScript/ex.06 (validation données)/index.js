d8b4279f676158c304a9535b7f3c45462a93127a
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-10-22 15:38:02 +0400

    suite ex validation

---
+++ Travaux Activité/15-OpenClassRooms JavaScript/ex.06 (validation données)/index.js
@@ -0,0 +1,19 @@
+let code = document.getElementById('code')
+let codeValide = document.getElementById('code-validation')
+let verife = document.getElementById('submit-btn')
+
+code.addEventListener('input', function(e) {
+    var value = e.target.value;
+
+    if (value.startsWith('CODE-')) {
+        verife.removeAttribute("disabled", "");
+        codeValide.innerHTML = value + ' : code valide !'
+        isValid = true;
+    } else {
+        verife.setAttribute("disabled", "");
+        codeValide.innerHTML = 'code invalide !'
+        isValid = false;
+    }
+});
+
+
