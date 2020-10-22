709bdf5b7cd48a80458375c5770c97f274a6ea58
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-10-22 14:17:38 +0400

    ex 05 AJAX

---
+++ Travaux Activité/15-OpenClassRooms JavaScript/ex.05 (AJAX serivce web)/script.js
@@ -0,0 +1,21 @@
+let meteoParis = document.getElementById('ask-weather')
+let resultatMeteo = document.getElementById('weather-result')
+
+function paskWeather() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            console.log(response)
+            console.log(response.current_condition.condition);
+            resultatMeteo.innerHTML = response.current_condition.condition
+        }
+    }
+    request.open("GET", "https://www.prevision-meteo.ch/services/json/paris");
+    request.send();
+}
+
+
+
+
+meteoParis.addEventListener('click', function() {paskWeather()})
