322a64d3dcdcc01fc99d6c3c45e4fc9b9ddc422d
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-11-05 07:17:10 +0400

    test pratiqur du cours

---
+++ Travaux Activité/15-OpenClassRooms JavaScript/ex.07 (save données)/script.js
@@ -0,0 +1,12 @@
+function send() {
+    var request = new XMLHttpRequest();//créer objet AJAX
+    var value = document.getElementById("value").value//récupérer valeur input champs
+    
+    request.open("POST", "https://mockbin.com/request");//request post avec url définis
+    request.setRequestHeader("Content-Type", "application/json");//header request
+    request.send(JSON.stringify(value));
+  }
+  
+  /*Event click sur btn pour lancer function */
+  let btnEnvoyer = document.getElementById('btn')
+  btnEnvoyer.addEventListener('click', function() {send()})
