37eb4b3a160e4fd402e4cbf1fe8d928261c32a09
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-11-05 07:35:20 +0400

    afficher resultat

--- Travaux Activité/15-OpenClassRooms JavaScript/ex.07 (save données)/script.js
+++ Travaux Activité/15-OpenClassRooms JavaScript/ex.07 (save données)/script.js
@@ -1,17 +1,20 @@
 function send() {
     var request = new XMLHttpRequest();//créer objet AJAX
     var value = document.getElementById("value").value//récupérer valeur input champs
+    var resultat = document.getElementById("result")//select id afficher résultat
     
     request.open("POST", "https://mockbin.com/request");//request post avec url définis
     request.setRequestHeader("Content-Type", "application/json");//header request
     request.send(JSON.stringify(value));
+
+    resultat.innerHTML = postData.text.value
 }
   
-/*Event click sur btn pour lancer function */
+//Event click sur btn pour lancer function 
 let btnEnvoyer = document.getElementById('btn')
 
 btnEnvoyer.addEventListener('click', function(event) {
     send()
-    event.preventDefault()/*eviter action défaut */
+    event.preventDefault()//éviter action défaut 
     event.stopPropagation()
 })
