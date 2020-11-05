c730920e51cf604c379f0b22253d363442e5666c
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-11-05 07:27:57 +0400

    ajout stopevent lors click btn

--- Travaux Activité/15-OpenClassRooms JavaScript/ex.07 (save données)/script.js
+++ Travaux Activité/15-OpenClassRooms JavaScript/ex.07 (save données)/script.js
@@ -5,8 +5,13 @@ function send() {
     request.open("POST", "https://mockbin.com/request");//request post avec url définis
     request.setRequestHeader("Content-Type", "application/json");//header request
     request.send(JSON.stringify(value));
-  }
+}
   
-  /*Event click sur btn pour lancer function */
-  let btnEnvoyer = document.getElementById('btn')
-  btnEnvoyer.addEventListener('click', function() {send()})
+/*Event click sur btn pour lancer function */
+let btnEnvoyer = document.getElementById('btn')
+
+btnEnvoyer.addEventListener('click', function(event) {
+    send()
+    event.preventDefault()/*eviter action défaut */
+    event.stopPropagation()
+})
